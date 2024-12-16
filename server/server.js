b0d03250c891c032970d68716d916fb53d109915
JonatanK-son <kontra.kson@gmail.com>
2024-12-16 11:06:35 +0100

    added express/server variables

---
+++ server/server.js
@@ -0,0 +1,17 @@
+express = require("express");
+server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+
+    next();
+  });
+
+server.listen(3000);
+
+server.get("/users", (req, res) => {});
